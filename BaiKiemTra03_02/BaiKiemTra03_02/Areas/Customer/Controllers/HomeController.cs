1c320413bbfc08d466c307f7c774bb7b04ee3b83
MinhThuan-dntu <thuannhox369@gmail.com>
2024-10-07 15:26:08 +0700

    BaiKiemTra03_02

---
+++ BaiKiemTra03_02/BaiKiemTra03_02/Areas/Customer/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BaiKiemTra03_02.Controllers
+{
+    [Area("Customers")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+    }
+}
